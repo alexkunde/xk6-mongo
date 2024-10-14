fec57dbaca4cd3ce30c2cc970793b244e5d66554
Adrian Ganseforth <adgan@mailbox.org>
2024-10-14 23:55:23 +0200

    Refactor test-aggregate.js, test-countdocuments.js, test-delete.js, test-deletemany.js, test-distinct.js, test-dropcollection.js, test-findall.js, test-findone.js, test-findoneandupdate.js, test-insert.js, test-insertmany.js, test-update.js, test-updatemany.js, and test-upsert.js

---
+++ examples/test-updatemany.js
@@ -0,0 +1,13 @@
+import xk6_mongo from 'k6/x/mongo';
+
+const client = xk6_mongo.newClient('mongodb://localhost:27017');
+const db = "testdb";
+const col = "testcollection";
+
+export default () => {
+    let results, error = client.updateMany(db, col, { correlationId: `test--mongodb` }, { locale: 'in', title: 'This is the change for all docs' })
+    if (error)
+        console.log(error.message)
+    else
+        console.log(`Updated Document: ${results}`);
+}
