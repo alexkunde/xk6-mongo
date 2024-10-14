f38e8bedc7981e8418f567a73c728748d2ed9205
Adrian Ganseforth <adgan@mailbox.org>
2024-10-14 23:31:54 +0200

    Refactor DeleteOne method in mongo.go to handle error cases and improve error logging

--- mongo.go
+++ mongo.go
@@ -220,10 +220,10 @@ func (c *Client) FindAll(database string, collection string) ([]bson.M, error) {
 func (c *Client) DeleteOne(database string, collection string, filter map[string]string) error {
 	db := c.client.Database(database)
 	col := db.Collection(collection)
-	log.Print(filter_is, filter)
-	result, err := col.DeleteOne(context.TODO(), filter)
+	result, err := col.DeleteOne(context.Background(), filter)
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("Error while deleting the document: %v", err)
+		return err
 	}
 	log.Printf("Deleted documents %v", result)
 	return nil
